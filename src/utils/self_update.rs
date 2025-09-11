a9d417305bc8e5afab464133967b51ab7d337267
Thierry Roland <thierryroland1411@yahoo.com>
2025-09-11 21:56:46 +0200

    display new version info only when new version has been downloaded

--- src/utils/self_update.rs
+++ src/utils/self_update.rs
@@ -4,20 +4,24 @@ use std::error::Error;
 
 pub fn self_update() {
     match try_update() {
-        Ok(new_version) => popup_info(format!("New version has been installed: {}", new_version)),
+        Ok((is_new_version, version)) => if is_new_version {
+            popup_info(format!("New version has been installed: {}", version))
+        },
         Err(e) => popup_error(format!("Update failed: {}", e)),
     };
 }
 
-fn try_update() -> Result<String, Box<dyn Error>> {
+fn try_update() -> Result<(bool, String), Box<dyn Error>> {
+    let version = env!("CARGO_PKG_VERSION");
     let status = Update::configure()
         .repo_owner("nashception")
         .repo_name("lister")
         .bin_name("lister")
-        .current_version(env!("CARGO_PKG_VERSION"))
+        .current_version(version)
         .no_confirm(true)
         .build()?
         .update()?;
 
-    Ok(status.version().to_string())
+    let new_version = status.version();
+    Ok((new_version > version, new_version.to_string()))
 }
