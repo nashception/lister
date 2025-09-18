4279d465963de506fd9cf456ee590c5dd73218aa
Thierry Roland <thierryroland1411@yahoo.com>
2025-09-18 12:02:59 +0200

    add spinner while indexing is in progress

--- src/domain/entities/directory.rs
+++ src/domain/entities/directory.rs
@@ -6,3 +6,13 @@ pub struct DirectoryData {
     pub drive_available_space: u64,
     pub directory: PathBuf,
 }
+
+impl DirectoryData {
+
+    pub fn file_name(&self) -> String {
+        self.directory
+            .file_name()
+            .map(|f| f.to_string_lossy().into_owned())
+            .unwrap_or_default()
+    }
+}
