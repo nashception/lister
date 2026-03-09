603f29a7a0d755146294eaec50f4a371937f23e4
Thierry Roland <thierryroland1411@yahoo.com>
2026-03-09 16:22:54 +0100

    add support for macOS

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use lister::infrastructure::updater::app_updater::self_update;
 use lister::ui::app::ListerApp;
 use lister::ui::app_factory::ListerAppService;
 
-#[cfg(not(any(target_os = "linux", target_os = "windows")))]
-compile_error!("This application only supports Linux and Windows");
+#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
+compile_error!("This application only supports Linux, macOS and Windows");
 
 fn main() -> iced::Result {
     self_update();
