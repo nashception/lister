55a2432ce6633ee959557986e3f58fd2592babf9
Thierry Roland <thierryroland1411@yahoo.com>
2026-03-16 08:18:31 +0100

    remove unnecessary entities

--- src/infrastructure/database/entities.rs
+++ src/infrastructure/database/entities.rs
@@ -1,36 +1,7 @@
+use crate::infrastructure::database::binary_format::UuidSqlite;
 use crate::infrastructure::database::schema::{drive_entries, file_categories, file_entries};
 use chrono::NaiveDateTime;
-use diesel::{Associations, Identifiable, Insertable, Queryable};
-use uuid::Uuid;
-use crate::infrastructure::database::binary_format::UuidSqlite;
-
-#[derive(Debug, Clone, PartialEq, Queryable, Identifiable)]
-#[diesel(table_name = file_categories)]
-struct FileCategoryEntity {
-    id: Uuid,
-    name: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Queryable, Identifiable, Associations)]
-#[diesel(belongs_to(FileCategoryEntity, foreign_key = category_id))]
-#[diesel(table_name = drive_entries)]
-struct DriveEntryEntity {
-    id: Uuid,
-    category_id: Uuid,
-    name: String,
-    available_space: i64,
-    insertion_time: NaiveDateTime,
-}
-
-#[derive(Debug, Clone, PartialEq, Queryable, Identifiable, Associations)]
-#[diesel(belongs_to(DriveEntryEntity, foreign_key = drive_id))]
-#[diesel(table_name = file_entries)]
-struct FileEntryEntity {
-    id: Uuid,
-    drive_id: Uuid,
-    path: String,
-    weight: i64,
-}
+use diesel::{Insertable, Queryable};
 
 #[derive(Queryable)]
 pub struct FileWithMetadataDto {
